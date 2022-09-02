84fff72fd944bd8a8112cd521dc207fc7c22e600
lihaohao <2495041749@qq.com>
2022-09-02 16:57:36 +0800

    feat: init project content

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// 引入共同的样式
+import "@/assets/css/reset.css";
+
+createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import jsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
+
+const resolvePath = (...urls: string[]) => path.resolve(__dirname, ...urls);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), jsx()],
+  resolve: {
+    alias: {
+      "@": resolvePath("src")
+    }
+  }
+});
