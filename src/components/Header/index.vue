2e89a7f3436d58a424ada0b61bc1ea9961c34949
lihaohao <2495041749@qq.com>
2022-09-09 07:42:31 +0800

    feat: 可以成功的设置 主题

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,20 +1,28 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import logo from "@/assets/images/logo.svg";
 
-// 表示模板flag
+// 表示主题
 const themeFlag = ref<boolean>(false);
+
+watch(themeFlag, (value: boolean) => {
+  const keyWorld = value ? "light" : "dark";
+  const el = document.querySelector("html") as HTMLElement;
+  el.setAttribute(`data-theme-rc`, keyWorld);
+});
 </script>
 
 <template>
-  <div class="header headBk">
-    <div class="header-left">
-      <img src="" alt="logo">
+  <div class="header headBk flex-between-center">
+    <div class="header-left flex-center">
+      <img :src="logo" alt="logo" />
     </div>
     <div class="header-middle"></div>
-    <div class="header-right">
+    <div class="header-right flex-center">
       <el-switch
         v-model="themeFlag"
         inline-prompt
+        style="--el-switch-off-color: #13ce66"
         size="large"
         active-text="护眼"
         inactive-text="明亮"
@@ -27,5 +35,18 @@ const themeFlag = ref<boolean>(false);
 @import "assets/css/common.less";
 .header {
   height: @headH;
+
+  .header-left {
+    > img {
+      width: 35px;
+      height: 35px;
+    }
+  }
+}
+
+.header-left,
+.header-right {
+  width: 80px;
+  height: 100%;
 }
 </style>
