7bfaa4e0f8e6a2a211b1c6092db2660a0257db8c
lihaohao <2495041749@qq.com>
2022-09-09 16:08:05 +0800

    feat: 对于画布部分 重新设置新的布局方式

--- src/views/Chart/index.vue
+++ src/views/Chart/index.vue
@@ -5,13 +5,35 @@ import Left from "@/views/Chart/components/Left/index.vue";
 <template>
   <div class="chart flex-direction-row full-height">
     <Left />
+    <div class="flex-1 chart-right flex-direction-column">
+      <div class="chart-right-top"></div>
+      <div class="chart-right-bottom">
+        <el-tooltip content="预览" placement="top">
+          <i class="iconfont icon-yulan pointer fc"></i>
+        </el-tooltip>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 @import "@/assets/css/common.less";
 
-.chart {
-  background: url('@/assets/images/canvas-bg.svg');
+.chart-right {
+  .chart-right-top {
+    flex: 1;
+    background: url("@/assets/images/canvas-bg.svg");
+    margin: 30px 30px 0px 30px;
+  }
+
+  .chart-right-bottom {
+    height: 50px;
+    padding: 0 30px;
+    .flex-fn(flex-end, center);
+
+    > i {
+      font-size: 20px;
+    }
+  }
 }
 </style>
