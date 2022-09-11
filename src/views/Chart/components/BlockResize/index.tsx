889197df93a18e37a578c38e20b53de9299cca83
lihaohao <2495041749@qq.com>
2022-09-11 18:05:31 +0800

    feat: 设置panel 可以拖拽

--- src/views/Chart/components/BlockResize/index.tsx
+++ src/views/Chart/components/BlockResize/index.tsx
@@ -1,11 +1,13 @@
-import { defineComponent, PropType } from 'vue'
-import './index.less'
-import {IPanelField} from "@/types";
+import { computed, defineComponent, PropType } from "vue";
+import "./index.less";
+import { IPanelField } from "@/types";
+import { useDragStore } from "@/store/drag";
+import store from "@/store";
 
 const enum IBlockMoveDirection {
-  START = 'START',
-  END = 'END',
-  CENTER = 'CENTER'
+  START = "START",
+  END = "END",
+  CENTER = "CENTER"
 }
 
 export default defineComponent({
@@ -16,9 +18,13 @@ export default defineComponent({
     }
   },
   setup(props) {
-    let data: Record<string, any> = {}
+    let data: Record<string, any> = {};
+    // 表示数据store
+    const store = useDragStore();
+    // 表示panel id
+    const panelId = computed<string>(() => props.curBlock?.id);
     const onmousemove = (e: MouseEvent) => {
-      let { clientX, clientY } = e
+      let { clientX, clientY } = e;
       let {
         startX,
         startY,
@@ -27,48 +33,65 @@ export default defineComponent({
         startLeft,
         startTop,
         direction
-      } = data
+      } = data;
 
       if (direction.horizontal === IBlockMoveDirection.CENTER) {
         // 如果拖拽的是 中间的点 X轴是不变的
-        clientX = startX
+        clientX = startX;
       }
       if (direction.vertical === IBlockMoveDirection.CENTER) {
         // 只能改横向 纵向是不发生变化的
-        clientY = startY
+        clientY = startY;
       }
 
-      let durX = clientX - startX
-      let durY = clientY - startY
+      let durX = clientX - startX;
+      let durY = clientY - startY;
 
       if (direction.vertical === IBlockMoveDirection.START) {
         //  针对反向拖拽的点 需要取反，拿到正确的组件top和left
-        durY = -durY
-        props.curBlock.top = startTop - durY
+        durY = -durY;
+        resizePanel({ top: startTop - durY });
       }
       if (direction.horizontal === IBlockMoveDirection.START) {
-        durX = -durX
-        props.curBlock.left = startLeft - durX
+        durX = -durX;
+        resizePanel({ left: startLeft - durX });
       }
 
-      const width = startWidth + durX
-      const height = startHeight + durY
+      const width = startWidth + durX;
+      const height = startHeight + durY;
 
-      props.curBlock.width = width
-      props.curBlock.height = height
-    }
+      resizePanel({ width, height });
+    };
+
+    // 表示执行flag
+    let execFlag = false;
+    /**
+     * @author lihh
+     * @description 共同的方法 修改panel 大小
+     * @param panel panel 面板
+     */
+    const resizePanel = (panel: Partial<IPanelField>) => {
+      if (execFlag) return;
+      execFlag = true;
+
+      requestAnimationFrame(() => {
+        store.editPanel(panelId.value, panel);
+        execFlag = false;
+      });
+    };
+
     const onmouseup = () => {
-      document.removeEventListener('mousemove', onmousemove)
-      document.removeEventListener('mouseup', onmouseup)
-    }
+      document.removeEventListener("mousemove", onmousemove);
+      document.removeEventListener("mouseup", onmouseup);
+    };
     const onmousedown = (
       e: MouseEvent,
       direction: {
-        horizontal: IBlockMoveDirection
-        vertical: IBlockMoveDirection
+        horizontal: IBlockMoveDirection;
+        vertical: IBlockMoveDirection;
       }
     ) => {
-      e.stopPropagation()
+      e.stopPropagation();
       data = {
         startX: e.clientX,
         startY: e.clientY,
@@ -77,10 +100,10 @@ export default defineComponent({
         startLeft: props.curBlock.left,
         startTop: props.curBlock.top,
         direction
-      }
-      document.addEventListener('mousemove', onmousemove)
-      document.addEventListener('mouseup', onmouseup)
-    }
+      };
+      document.addEventListener("mousemove", onmousemove);
+      document.addEventListener("mouseup", onmouseup);
+    };
 
     return () => (
       <>
@@ -149,6 +172,6 @@ export default defineComponent({
             })
           }></div>
       </>
-    )
+    );
   }
-})
+});
